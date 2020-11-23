b4ded7b74bba677552c83508b88137943530061a
pequenojoohn <kheymk@hotmail.com>
2020-11-23 15:01:58 -0300

    Finish update product.

--- src/components/ProductDetail/index.js
+++ src/components/ProductDetail/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 
 import { BiChevronLeft } from 'react-icons/bi';
 
@@ -15,6 +15,7 @@ const ProductDetail = (props) => {
 
     const [product, setProduct] = useState([0]);
     const [edit, setEdit] = useState(false);
+    const history = useHistory();
 
     const [quantity, setQuantity] = useState('');
     const [salePrice, setSalePrice] = useState('');
@@ -23,23 +24,49 @@ const ProductDetail = (props) => {
     const [height, setHeight] = useState('');
     const [width, setWidth] = useState('');
     const [depth, setDepth] = useState('');
-
+    
     async function handleModify(e) {
         e.preventDefault();
-
-        const updatedQuery = `{ mutation { updateSku(id:${productId}, quantity:${quantity}, salePrice: ${salePrice},){ quantity }}}`;
-
-        try {
-            await api.post(`?query=${updatedQuery}`).then(response => {
-                console.log(response);
+        
+        const updatedQuery = `mutation { updateSku(
+            id: ${productId}, 
+            quantity: ${!quantity ? product.quantity : quantity}, 
+            salePrice: ${!salePrice ? product.salePrice : salePrice}, 
+            promotionalPrice: ${!promotionalPrice ? product.promotionalPrice : promotionalPrice}, 
+            package: {
+                weight: ${!weight ? product.package?.weight : weight}, 
+                height: ${!height ? product.package?.height : height}, 
+                width: ${!width ? product.package?.width : width}, 
+                depth: ${!depth ? product.package?.depth : depth}
             })
-        } catch (err) {
-            alert(err)
+            {
+                id
+                quantity 
+                salePrice 
+                promotionalPrice
+                package
+            }}`
+            
+            try {
+                await api.post(`?query=${updatedQuery}`).then(response => {
+                    console.log(response);
+                    editProduct();
+                    history.go(0)
+                });
+            } catch (err) {
+                alert(err)
+            }
         }
-    }
-
+        
     function editProduct() {
         setEdit(!edit);
+        setQuantity('');
+        setSalePrice('');
+        setPromotionalPrice('');
+        setWeight('');
+        setHeight('');
+        setWidth('');
+        setDepth('');
     }
 
     useEffect(() => {
@@ -68,13 +95,13 @@ const ProductDetail = (props) => {
 
                             {edit ?
                                 <>
-                                    <p>Estoque:<input defaultValue={product.quantity} value={quantity} onChange={e => setQuantity(e.target.value)} /></p>
-                                    <p>Preço de venda:<input defaultValue={product.salePrice} value={salePrice} onChange={e => setSalePrice(e.target.value)} /></p>
-                                    <p>Preço Promocional:<input defaultValue={product.promotionalPrice} value={promotionalPrice} onChange={e => setPromotionalPrice(e.target.value)} /></p>
-                                    <p>Peso: <input defaultValue={product.package?.weight} value={weight} onChange={e => setWeight(e.target.value)} /></p>
-                                    <p>Altura: <input defaultValue={product.package?.height} value={height} onChange={e => setHeight(e.target.value)} /></p>
-                                    <p>Largura: <input defaultValue={product.package?.width} value={width} onChange={e => setWidth(e.target.value)} /></p>
-                                    <p>Profundidade: <input defaultValue={product.package?.depth} value={depth} onChange={e => setDepth(e.target.value)} /></p>
+                                    <p>Estoque:<input placeholder={product.quantity} value={quantity} onChange={e => setQuantity(e.target.value)} /></p>
+                                    <p>Preço de venda:<input placeholder={product.salePrice} value={salePrice} onChange={e => setSalePrice(e.target.value)} /></p>
+                                    <p>Preço Promocional:<input placeholder={product.promotionalPrice} value={promotionalPrice} onChange={e => setPromotionalPrice(e.target.value)} /></p>
+                                    <p>Peso: <input placeholder={product.package?.weight} value={weight} onChange={e => setWeight(e.target.value)} /></p>
+                                    <p>Altura: <input placeholder={product.package?.height} value={height} onChange={e => setHeight(e.target.value)} /></p>
+                                    <p>Largura: <input placeholder={product.package?.width} value={width} onChange={e => setWidth(e.target.value)} /></p>
+                                    <p>Profundidade: <input placeholder={product.package?.depth} value={depth} onChange={e => setDepth(e.target.value)} /></p>
                                 </>
                                 :
                                 <>
